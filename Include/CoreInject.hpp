691d3f4a1a45d7d6eb12f70df0ac8b51a5942715
johannes <johannesmiesenhardt@gmail.com>
2024-02-16 15:27:37 +0100

    Isolate settings into their own module

--- Include/CoreInject.hpp
+++ Include/CoreInject.hpp
@@ -9,59 +9,9 @@
 #include <string>
 #include <vector>
 
+#include "CoreInject/Settings.hpp"
+
 namespace CoreInject {
-	struct Flag {
-		std::string name;
-		std::string description;
-		Flag* dependant = nullptr;
-		bool state = false;
-
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "google-explicit-constructor"
-		operator bool() const { return state; }
-#pragma clang diagnostic pop
-	};
-
-	struct Settings {
-		Flag allowDoubleInjection{
-			"allowDoubleInjection",
-			"If enabled, then modules that are already loaded by the target process wont be filtered\n"
-			"Note: Turning this off might not filter accurately because the library might be renamed when 'workaroundSandboxes' is used without 'overwriteRelocationTarget'"
-		};
-
-		Flag workaroundSandboxes{
-			"workaroundSandboxes",
-			"Before injection, move the library to the current working directory of the target process"
-		};
-
-		Flag overwriteRelocationTarget{
-			"overwriteRelocationTarget",
-			"If the target file already exists, then it will be overwritten",
-			&workaroundSandboxes
-		};
-
-		Flag deleteAfterInjection{
-			"deleteAfterInjection",
-			"Deletes the module after injection. In case of a sandbox workaround, this affects the relocated module, not the original"
-		};
-
-		static constexpr std::tuple allSettings{
-			&Settings::allowDoubleInjection,
-			&Settings::workaroundSandboxes,
-			&Settings::overwriteRelocationTarget,
-			&Settings::deleteAfterInjection
-		};
-
-		template <typename F, std::size_t Idx = std::tuple_size_v<decltype(allSettings)> - 1>
-		constexpr void forEachSetting(F f)
-		{
-			if constexpr (Idx > 0)
-				forEachSetting<F, Idx - 1>(f);
-
-			f(*this.*(std::get<Idx>(allSettings)));
-		}
-	};
-
 	using Module = std::filesystem::path;
 
 	class CoreInject {
@@ -72,11 +22,10 @@ namespace CoreInject {
 		void relocateModules(std::vector<Module>& modules) const;
 
 	public:
-		explicit CoreInject(std::size_t targetProcess,
-			Settings  settings);
+		explicit CoreInject(std::size_t targetProcess, Settings settings);
 
 		[[nodiscard]] std::size_t run(std::vector<Module> modules) const;
 	};
-};
+}
 
 #endif
---
+++ Settings/Include/CoreInject/Settings.hpp
@@ -0,0 +1,61 @@
+#ifndef COREINJECT_SETTINGS_HPP
+#define COREINJECT_SETTINGS_HPP
+
+#include <string>
+
+namespace CoreInject {
+
+	struct Flag {
+		std::string name;
+		std::string description;
+		Flag* dependant = nullptr;
+		bool state = false;
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "google-explicit-constructor"
+		operator bool() const { return state; }
+#pragma clang diagnostic pop
+	};
+
+	struct Settings {
+		Flag allowDoubleInjection{
+			"allowDoubleInjection",
+			"If enabled, then modules that are already loaded by the target process wont be filtered\n"
+			"Note: Turning this off might not filter accurately because the library might be renamed when 'workaroundSandboxes' is used without 'overwriteRelocationTarget'"
+		};
+
+		Flag workaroundSandboxes{
+			"workaroundSandboxes",
+			"Before injection, move the library to the current working directory of the target process"
+		};
+
+		Flag overwriteRelocationTarget{
+			"overwriteRelocationTarget",
+			"If the target file already exists, then it will be overwritten",
+			&workaroundSandboxes
+		};
+
+		Flag deleteAfterInjection{
+			"deleteAfterInjection",
+			"Deletes the module after injection. In case of a sandbox workaround, this affects the relocated module, not the original"
+		};
+
+		static constexpr std::tuple allSettings{
+			&Settings::allowDoubleInjection,
+			&Settings::workaroundSandboxes,
+			&Settings::overwriteRelocationTarget,
+			&Settings::deleteAfterInjection
+		};
+
+		template <typename F, std::size_t Idx = std::tuple_size_v<decltype(allSettings)> - 1>
+		constexpr void forEachSetting(F f)
+		{
+			if constexpr (Idx > 0)
+				forEachSetting<F, Idx - 1>(f);
+
+			f(*this.*(std::get<Idx>(allSettings)));
+		}
+	};
+}
+
+#endif
